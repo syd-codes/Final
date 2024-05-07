66c0708fa63f821a6a8d012d737ab8001f89e5df
Sydney <sydneygcrowder@gmail.com>
2024-05-07 10:18:25 -0600

    All done : final app

---
+++ Program/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net;
+
+namespace BirthdayCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("What is your birthdate? Please formate it in YYYY-MM-DD");
+            string response = Console.ReadLine();
+            while (true)
+            {
+                if(DateTime.TryParse(response, out var birthday))
+                {
+                    TimeSpan age = DateTime.Now - birthday;
+                    int ageInDays = (int)age.TotalDays;
+
+                    Console.WriteLine($"Your age in days it {ageInDays} ");
+
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Something when wrong, Please try again. Formatting it YYYY-MM-DD");
+                }
+                Console.WriteLine("Press any key to continue");
+
+            }
+        }
+    }
+
+}
